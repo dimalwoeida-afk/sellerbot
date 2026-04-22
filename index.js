4d5aa782b44c8745c753e489e289739a68c1b4e0
dimalwoeida-afk <dimalwoeida@gmail.com>
2026-04-22 20:10:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Web server running on port ${PORT}`);
 });
-require("dotenv").config();
+
 const fs = require("fs");
 const path = require("path");
 const {
